9dac9992c16a178b85eae52514c3c44b77a8abe1
patrick hrabos <phrabos@gmail.com>
2021-06-05 14:54:35 -0400

    merged the wrong branch previously...

--- challenge.js
+++ challenge.js
@@ -1,45 +1,39 @@
-function getCrewSize(id){
-  const starships = [];
-  fetch(`https://swapi.dev/api/people/${id}`)
-    .then(res => res.json())
-    .then(parsed => starships.push(parsed.starships))
-    // .then(parsed => console.log(parsed))
-    // .then(json => starships.push(json.sharships))
-    // .then(json => console.log(json))
-  console.log(starships)
+async function getCrewSize(id){
+  const res = await fetch(`https://swapi.dev/api/people/${id}`)
+  const { starships } = await res.json();
+  console.log('starships', starships);
+  if(starships.length === 0) return 0
 
-  // if(starships.length === 0) return 0
-    
-  //   const shipPromiseArr = []
+    const shipPromiseArr = []
 
-  // starships.forEach(ship => {
-  //   console.log('ship', ship)
-  //   const fetchShip = fetch(ship)
-  //   console.log('fetchShip', fetchShip)
-  //   shipPromiseArr.push(fetchShip)
-  // })
-  // console.log('shipPromiseArr', shipPromiseArr)
+  starships.forEach(ship => {
+    console.log('ship', ship)
+    const fetchShip = fetch(ship)
+    console.log('fetchShip', fetchShip)
+    shipPromiseArr.push(fetchShip)
+  })
+  console.log('shipPromiseArr', shipPromiseArr)
 
-  // const resultsArr = await Promise.all(shipPromiseArr);
-  // const parsedResultsProm = []
-  // resultsArr.forEach(unparsed => {
-  //   const res = unparsed.json()
-  //   parsedResultsProm.push(res)
-  // })
-  // const parsedResults = await Promise.all(parsedResultsProm)
-  // console.log('parsed', parsedResults)
+  const resultsArr = await Promise.all(shipPromiseArr);
+  const parsedResultsProm = []
+  resultsArr.forEach(unparsed => {
+    const res = unparsed.json()
+    parsedResultsProm.push(res)
+  })
+  const parsedResults = await Promise.all(parsedResultsProm)
+  console.log('parsed', parsedResults)
 
-  // const crew = parsedResults.reduce((acc, curr) => {
-  //   if(curr.crew === 'unknown'){
-  //     return acc + 1
-  //   }
-  //   return acc + +curr.crew
-  // }, 0);
+  const crew = parsedResults.reduce((acc, curr) => {
+    if(curr.crew === 'unknown'){
+      return acc + 1
+    }
+    return acc + +curr.crew
+  }, 0);
 
-  // console.log(crew)
+  console.log(crew)
 
-  return parsed;
+  return crew;
 
 }
-console.log('result', getCrewSize(11))
-  
+getCrewSize(11)
+  .then(res => console.log('result', res))
