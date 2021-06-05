cd445345386d9aae792953bea6bff8a1586365cb
patrick hrabos <phrabos@gmail.com>
2021-06-05 13:31:09 -0400

    2nd attempt success!

--- challenge.js
+++ challenge.js
@@ -1,37 +1,39 @@
 async function getCrewSize(id){
   const res = await fetch(`https://swapi.dev/api/people/${id}`)
   const { starships } = await res.json();
-  console.log(starships);
-
-  const fetchArr = []
-
-  for(let i = 0; i < starships.length; i++){
-    const ship = starships[i].toString();
+  console.log('starships', starships);
+  if(starships.length === 0) return 0
     
+    const shipPromiseArr = []
+
+  starships.forEach(ship => {
+    console.log('ship', ship)
     const fetchShip = fetch(ship)
-    fetchArr.push(fetchShip)
-  }
+    console.log('fetchShip', fetchShip)
+    shipPromiseArr.push(fetchShip)
+  })
+  console.log('shipPromiseArr', shipPromiseArr)
 
-  // starships.forEach(ship => {
-  //   console.log('ship', ship)
-  //   const fetchShip = fetch(ship)
-  //   console.log('fetchShip', fetchShip)
-  //   fetchArr.push(fetchShip)
-  // })
-  console.log('fetcharr', fetchArr)
+  const resultsArr = await Promise.all(shipPromiseArr);
+  const parsedResultsProm = []
+  resultsArr.forEach(unparsed => {
+    const res = unparsed.json()
+    parsedResultsProm.push(res)
+  })
+  const parsedResults = await Promise.all(parsedResultsProm)
+  console.log('parsed', parsedResults)
 
-  const resultsArr = await Promise.all(fetchArr);
- 
+  const crew = parsedResults.reduce((acc, curr) => {
+    if(curr.crew === 'unknown'){
+      return acc + 1
+    }
+    return acc + +curr.crew
+  }, 0);
 
-  console.log('resArr', resultsArr)
+  console.log(crew)
 
-  // for(let i = 0; i < resultsArr.length; i++){
-  //   const shipDetails = 
-  // }
-
-
-  return starships;
+  return crew;
 
 }
-getCrewSize(1)
-  // .then(res => console.log(res))
+getCrewSize(11)
+  .then(res => console.log('result', res))
